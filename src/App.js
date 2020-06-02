fafc4b7e6649fbd84c938e6bd186f964c90ba330
Dmitriy Zatulovskiy <63239969+GrafSoul@users.noreply.github.com>
2020-06-02 13:05:01 +0500

    feat: Added project files.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import RPSGame from './RPSGame/RPSGame'
+
+function App() {
+    return ( 
+        <div className="app">
+        <RPSGame />
+        </div>
+    );
+}
+
+export default App;
